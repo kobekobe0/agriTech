ad7b1df500f119ac4f6f78ba601a228a026fdf98
kobekobe0 <kobe0santos0@gmail.com>
2021-10-08 11:45:36 +0800

    color

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { CgProfile } from 'react-icons/cg'
 import './header.css'
+import { Link, useLocation } from 'react-router-dom'
 
-function Header(props) {
+function Header() {
+    const location = useLocation()
+    console.log(location.pathname)
     return (
         <div className="header">
-            <h1>Title</h1>
+            {location.pathname == '/' ? <h1>Map</h1> : null}
+            {location.pathname == '/action' ? <h1>Tasks</h1> : null}
+            {location.pathname == '/analytics' ? <h1>Analytics</h1> : null}
             <CgProfile size={40} />
         </div>
     )
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './nav.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { BsMapFill } from 'react-icons/bs'
 import { FaTractor } from 'react-icons/fa'
 import { AiOutlineAreaChart } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 function Nav() {
+    const location = useLocation()
+    console.log(location.pathname)
     return (
         <motion.div
             className="itemContainer"
@@ -14,17 +16,79 @@ function Nav() {
             exit={{ top: -100 }}
             className="nav"
         >
-            <Link to="/">
+            <Link
+                to="/"
+                style={{
+                    borderBottom:
+                        location.pathname == '/'
+                            ? 'rgb(105, 0, 131) 2px solid'
+                            : 'black 2px solid',
+                }}
+            >
                 <BsMapFill size={21} />
-                <h2>My Farm</h2>
+                <h2
+                    style={{
+                        color:
+                            location.pathname == '/'
+                                ? 'rgb(105, 0, 131)'
+                                : 'black',
+                        fontWeight: location.pathname == '/' ? 'bold' : 'unset',
+                    }}
+                >
+                    My Farm
+                </h2>
             </Link>
-            <Link to="/action">
+            <Link
+                to="/action"
+                style={{
+                    borderBottom:
+                        location.pathname == '/action'
+                            ? 'rgb(105, 0, 131) 2px solid'
+                            : 'black 2px solid',
+                }}
+            >
                 <FaTractor size={27} />
-                <h2>Action</h2>
+                <h2
+                    style={{
+                        color:
+                            location.pathname == '/action'
+                                ? 'rgb(105, 0, 131)'
+                                : 'black',
+                        fontWeight:
+                            location.pathname == '/action' ? 'bold' : 'unset',
+                    }}
+                >
+                    Action
+                </h2>
             </Link>
-            <Link to="/analytics" className="active">
+            <Link
+                to="/analytics"
+                style={{
+                    borderBottom:
+                        location.pathname == '/analytics'
+                            ? 'rgb(105, 0, 131) 2px solid'
+                            : 'black 2px solid',
+                    color:
+                        location.pathname == '/analytics'
+                            ? 'rgb(105, 0, 131)'
+                            : 'black',
+                }}
+            >
                 <AiOutlineAreaChart size={27} />
-                <h2>Analytics</h2>
+                <h2
+                    style={{
+                        color:
+                            location.pathname == '/analytics'
+                                ? 'rgb(105, 0, 131)'
+                                : 'black',
+                        fontWeight:
+                            location.pathname == '/analytics'
+                                ? 'bold'
+                                : 'unset',
+                    }}
+                >
+                    Analytics
+                </h2>
             </Link>
         </motion.div>
     )
